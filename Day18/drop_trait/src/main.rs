400d32dbd9c2226aa1b4025d57ae0acedbfc7962
deep0072 <deepakumar50136@gmail.com>
2024-02-04 15:47:25 +0530

    drop trait for smartpointer

--- Day18/drop_trait/src/main.rs
+++ Day18/drop_trait/src/main.rs
@@ -1,12 +1,12 @@
 // drop trait =>> it is mostly used to free up the memory,e files or network connections
 // it is called by compiler by default if we just mention drop function
 
-struct CustomSmartPointer{
-    data:String
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl Drop for  CustomSmartPointer{
-    fn drop(&mut self){
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
         println!("droppping from custome pointer {}", self.data);
     }
 }
@@ -14,15 +14,18 @@ impl Drop for  CustomSmartPointer{
 fn main() {
     println!("Hello, world!");
 
-    let first_Smart_pointer = CustomSmartPointer{data:String::from("first smartpointer")};
-    drop(first_Smart_pointer);
-
-    let second_Smart_pointer = CustomSmartPointer{data:String::from("second smartpointer")};
-    // after above line drop function will be called by compiler
-    // variable dropping occure in reverse order. 'second_Smart_pointer' will be dropped first 
-
-    //------------------------------------------------------------------------------------------------------
+    let first_Smart_pointer = CustomSmartPointer {
+        data: String::from("first smartpointer"),
+    };
 
     // lets say for some reason we wanted to delete 'first_Smart_pointer' then we need to use  std::mem::drop
-  
+    drop(first_Smart_pointer); // it will dropped first and then
+
+    let second_Smart_pointer = CustomSmartPointer {
+        data: String::from("second smartpointer"),
+    };
+    // after above line drop function will be called by compiler
+    // variable dropping occure in reverse order. 'second_Smart_pointer' will be dropped first
+
+    //------------------------------------------------------------------------------------------------------
 }
