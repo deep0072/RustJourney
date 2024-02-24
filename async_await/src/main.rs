c7ca0ae51e53daa513fd3f1ce298803dfaad70d0
deep0072 <deepakumar50136@gmail.com>
2024-02-24 23:19:16 +0530

    async await function example via http request

---
+++ async_await/src/main.rs
@@ -0,0 +1,19 @@
+use error_chain::error_chain;
+
+error_chain!{
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error); // handle http request errors 
+    }
+}
+
+#[tokio::main] // tokio used for async function t
+async fn main()-> Result<()> {
+    let res = reqwest::get("http://httpbin.org/get").await?;
+    println!("status {}", res.status());
+    println!("headers:\n {:?}", res.headers());
+
+    let body = res.text().await?;
+    println!("body {}", body);
+    Ok(())
+}
