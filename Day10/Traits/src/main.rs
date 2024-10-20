8b116d0c6703dbc21364636a37498ac0fbaab60d
deep0072 <deepakumar50136@gmail.com>
2024-10-20 22:55:04 +0530

    added traits

--- Day10/Traits/src/main.rs
+++ Day10/Traits/src/main.rs
@@ -1,95 +1,218 @@
-use std::fmt::format;
+// traits ==> are the kind of interfaces
+// means common  functions structure that can be 
+// used at different places
+
+use std::fmt::{Display, Debug};
+
+trait Summary {
+
+    fn get_author(&self) -> &str;
 
 
-pub trait Summary  {
+    // this function will be called by any struct by default
+    fn summarize(&self) -> String{
+        
+        format!("{} read more...", self.get_author())
 
-    fn summarize_author(&self)->String; // it need to be mention in child block
-
-    fn summarize(&self)->String{
-        format!("Hi there Read more..{}",self.summarize_author()) // even if not call this still it will run child block
     }
     
 }
+
+struct NewsArticle{
+    pub headline:String,
+    pub location: String,
+    pub author: String,
+    pub content: String
+}
+
+impl Summary for NewsArticle {
+
+    // if summarize function not defined with its body then it need to be define here
+
+    // fn summarize(&self) -> String {
+    //     format!("author: {}, headline: {}, news: {}", self.author,self.headline,self.content)
+    // }
+
+    //-------------------------------------
+
+    fn get_author(&self) -> &str {
+
+        self.author.as_str()
+        // let news_summary = format!("author: {}, headline: {}, news: {}", self.author,self.headline,self.content);
+
+        // news_summary.as_str()
+
+
+        
+    }
+}
+
+trait  MyTrait {
+    fn demo(&self) {
+        println!("hi from demo trait")
+    }
     
-struct NewsArticle {
-    Headline:String,
-    Location:String,
-    Author:String
-}
-
-impl Summary for NewsArticle{
-    fn summarize_author(&self)->String {
-        format!("hi {}", self.Author)
-    }
-
-    //lets say if we keep this impl block empty then Summary function output will be the default result that is "Hi there Read more.."
-    fn summarize(&self)->String {
-        format!("{}, by {}", self.Headline, self.Author)
-    }
-
-    // if this function not defined in traits block then we need to define it otherwise not .
-
 }
 
 
-pub struct Tweet {
-    pub username:String,
-    pub content:String,
-    pub reply:bool,
-    pub retweet:bool
+struct Tweet{
+    username: String,
+    content:String,
+    reply: bool,
+    retweet:bool
 }
+
+// implement trait for 
 
 impl Summary for Tweet {
-    fn summarize_author(&self)->String {
-        format!("{} by {}", self.content,self.username)
+    // need to add same function we used in Summary trait
+    // if summarize function not defined with its body then it need to be define here
+    // otherwise summarize trait's function will be called by default
+    // fn summarize(&self) -> String {
+    //     format!("tweet:{}, {}", self.username,self.content)
+        
+    // }
+
+    //------------------------------------------
+
+    fn get_author(&self) -> &str {
+        // let  tweet = format!("tweet:{}, {}", self.username,self.content);
+        self.username.as_str()
     }
-    // summarize function already mention in trait block then it will be override in this block
-    fn summarize(&self)->String {
-        format!("{}", self.username)
-    }
+
+    //summarize function called by default which call the get_author function
 }
 
-// traits as params
-pub fn notify(item: &impl Summary){
-    println!("Breaking News! {}", item.summarize())
-}
+impl MyTrait for Tweet{}
+
 
 
 fn main() {
     println!("Hello, world!");
 
-    let tweet_struct = Tweet{
+    
+
+    let tweet = Tweet {
         username:String::from("tech_update0"),
-        content:String::from("hi there"),
-        reply:true,
-        retweet:true
-
-
+        content: String::from("hi am  learning rust "),
+        reply:false,
+        retweet:false
+    };
+    let tweet2 = Tweet {
+        username:String::from("tech_update02 output with implementation of trait bound"),
+        content: String::from("output with implementation of trait bound"),
+        reply:false,
+        retweet:false
+    };
+    let news_article = NewsArticle {
+        headline:String::from("lorence bishnoi sbke lore lgade"),
+        location: String::from("hi am  learning rust "),
+        author:String::from("deepak"),
+        content:String::from("lorence bishnoi hua khatarnak")
     };
 
-    let news_struct = NewsArticle{
-        Headline:String::from("check this headline"),
-        Location:String::from("delhi"),
-        Author:String::from("deep")
-    };
+    // as aggregator function accept summary trait 
+    // if we pass tweet struct then it accept cause tweet struct implement Summary trait
 
-//    println!("tweet summary {}", tweet_struct.summarize());
-//    println!("News Article summary {}", news_struct.summarize());
-   notify(&news_struct);
-   println!("{}" , returns_summarize().summarize());
+    // aggregator(tweet);
+    // aggregator(news_article);
+    // get_news(tweet2);
+
+    //-----mixing multiple traited bound struct
+    mix_up_news(&tweet, &news_article);
+    mixup_trait(&tweet);
+
+    //-----------------------------
+
+    /*
+     this wont work here because it doesnot implement Mytrait
+    
+     */
+    // mixup_trait(&news_article);
+
+
 
 
 }
 
-// lets create function that return type "impl Summary"
+// now lets create function the aggregate the info from NewsArticle as well as from tweeter
 
-fn returns_summarize() -> impl Summary{
-    Tweet {
-        username:String::from("techy"),
-        content:String::from("techy"),
-        reply:true,
-        retweet:true
-    }
+// source: impl Summary ==> traits as parameter passed in aggregator
 
-    // this return Summary type data then after that return we can access the Summary function or methods
+fn aggregator(source: impl Summary) {
+    // impl Summary =>> means we are implementing Summary trait
+
+    // Tweet struct uses Summary trait
+
+
+    println!("{}", source.summarize());
+
 }
+
+//--------------------------trait bound-------------
+// fn get_news<T>(source:T){
+//     println!("{}", source.summarize())
+
+//     // as in this function we are trying to call summarize()
+//     // but we do not know whats the T here.
+//     // we need to use bound where we can tell that T is only refer to Summary Trait
+// }
+fn get_news<T: Summary>(source:T){
+    println!("{}", source.summarize())
+
+    // as in this function we are trying to call summarize()
+    // but we do not know whats the T here.
+    // we need to use bound where we can tell that T is only refer to Summary Trait
+}
+
+// pass multiple argument with traits
+
+/*  but this wont work thouh botht implements Summary trait
+but each struct each is differnet instance of struct it self
+
+*/
+
+// fn mix_up_news<T:Summary>(source1: &T, source2: &T){
+//     println!("source1 {}, source2 {}", source1.summarize(), source2.summarize())
+
+// }
+
+
+fn mix_up_news(source1: &impl Summary, source2: &impl Summary){
+    println!("source1 {}, source2 {}", source1.summarize(), source2.summarize())
+
+}
+
+// ---------------------------------------------
+
+
+/*
+let say tweet struct uses multiple trait and we wanted to show 
+all its trait's function in function 
+
+
+*/
+
+/*
+params are saying that pass those 
+parameter who implements both trait  Summary and MyTrait
+
+*/
+fn mixup_trait(source: &(impl Summary + MyTrait)) {
+
+    println!("summary trait {}, Mytrait is {:?}", source.summarize(), source.demo())
+
+}
+
+
+//-----------------------------------------
+
+// we can also use which generics implements trait bound
+
+// fn some_function<T,U>(t: &T, u:&U)  -> i32 
+// where  T:Display + Clone, U: Clone + Debug {
+
+// }
+
+
+
